a189651ecc8aedfcf8fc9e80dabaee126f5c7626
MarcAhoy <65318265+MarcAhoy@users.noreply.github.com>
2022-07-22 10:44:35 -0700

    Create opener.js

---
+++ opener.js
@@ -0,0 +1,1 @@
+javascript:void(window.open('http://patokeep.epizy.com/popup.php','popupppp', 'width=300,height=450,left=100,top=100,resizable=no,toolbar=no,location=no,scrollbars=yes,status=no,menubar=no'));
